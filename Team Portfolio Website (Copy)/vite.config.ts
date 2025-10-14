fb68c6a1c970a786efbe3fb42bd438a3ee12f757
Olav Liljeberg <sofnbeat@gmail.com>
2025-10-15 01:54:55 +0200

    update node

--- Team Portfolio Website (Copy)/vite.config.ts
+++ Team Portfolio Website (Copy)/vite.config.ts
@@ -2,6 +2,7 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+  import { fileURLToPath, URL } from 'node:url';
 
   export default defineConfig({
     base: '/syntax-flow2/',
@@ -47,7 +48,7 @@
         '@radix-ui/react-aspect-ratio@1.1.2': '@radix-ui/react-aspect-ratio',
         '@radix-ui/react-alert-dialog@1.1.6': '@radix-ui/react-alert-dialog',
         '@radix-ui/react-accordion@1.2.3': '@radix-ui/react-accordion',
-        '@': path.resolve(__dirname, './src'),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
     build: {
